a907d54ca2240e9cef1b228f3517af2d33a754fd
raymond gitonga <raytosh95@gmail.com>
2019-08-27 14:24:39 +0300

    fix

--- app.js
+++ app.js
@@ -11,33 +11,33 @@ app.use(express.json());
 
 //None of the fields should be empty
 const params = {
-    //The organization shortcode used to receive the transaction.
-    'BusinessShortCode': '', 
-    //The transaction type to be used for this request.
-    'TransactionType': 'CustomerPayBillOnline', 
-    //Amount to be charged / paid
-    'Amount': '1', 
-    //The mobile number sending the funds. When testing use shortcode 1 from https://developer.safaricom.co.ke/test_credentials
-    'PartyA': '', 
-    // The organization shortcode receiving the funds. When testing use from Lipa Na Mpesa Online Shortcode:
-    'PartyB': '', 
-    // The mobile number sending the funds.
-    'PhoneNumber': '', 
-    // The url to where responses from M-Pesa will be sent to.
-    'CallBackURL': 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest', 
-    //Name will display to the one paying
-    'AccountReference': '', 
-    'TransactionDesc': 'Testing mpesa online',
-    //consumer key
-    'consumerKey': '', 
-    // consumer secret
-    'consumerSecret': '', 
-    // Used to create a password for use when making a Lipa Na M-Pesa Online Payment API calls generate yours from https://developer.safaricom.co.ke/test_credentials
-    'passKey': '', 
-    // MPESA authentication end point
-    'authenticationURL': 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials', 
-    // MPESA request processing end point
-    'processRequestURL': 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest' 
+    'BusinessShortCode': '',  //The organization shortcode used to receive the transaction.
+    
+    'TransactionType': 'CustomerPayBillOnline', //The transaction type to be used for this request.
+    
+    'Amount': '1', //Amount to be charged / paid
+    
+    'PartyA': '', //The mobile number sending the funds. When testing use shortcode 1 from https://developer.safaricom.co.ke/test_credentials
+    
+    'PartyB': '', // The organization shortcode receiving the funds. When testing use from Lipa Na Mpesa Online Shortcode:
+    
+    'PhoneNumber': '', // The mobile number sending the funds.
+    
+    'CallBackURL': 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest', // The url to where responses from M-Pesa will be sent to.
+    
+    'AccountReference': '', //Name will display to the one paying
+    
+    'TransactionDesc': 'Testing mpesa online', //Description of transaction
+    
+    'consumerKey': '', // your consumer key
+    
+    'consumerSecret': '', // your consumer secret
+    
+    'passKey': '', // Used to create a password for use when making a Lipa Na M-Pesa Online Payment API calls generate yours from https://developer.safaricom.co.ke/test_credentials
+    
+    'authenticationURL': 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials', // MPESA authentication end point
+    
+    'processRequestURL': 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest' // MPESA request processing end point
   }
 
 
